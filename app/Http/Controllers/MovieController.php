5b2410163700d9c45e37600801f46ce80b0143fc
Vito Farenga <vitofarenga@libero.it>
2022-09-08 16:31:29 +0200

    non trova movie

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     //
+    public function index(){
+        $movies = Movie::all();
+        return view('home', compact('$movies'));
+    }
 }
